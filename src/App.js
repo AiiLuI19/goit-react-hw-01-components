e94e111cb34d9216a0eca91352177af31a84708d
Iuliia <iuliiaiavorskaia@gmail.com>
2022-06-28 15:39:18 +0300

    addfix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Profile from './components/Profile/user';
 import user from './components/Profile/user.json';
 import data from './components/Statistics/data.json';
-import Statistics from './components/Statistics/Statistics';
+import Statistics from './components/Statistics/statistics';
 import friends from './components/FriendList/friends.json';
 import FriendList from './components/FriendList/FriendList';
 import transactions from './components/TransactionHistory/transactions.json';
@@ -18,9 +18,7 @@ export default function App() {
         views={user.stats.views}
         likes={user.stats.likes}
       />
-
       <Statistics title="Upload stats" stats={data} />
-
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </div>
